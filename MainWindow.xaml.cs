7ba07b68c9f95e5347fb8a1cb157cd2764e9c5d4
Patrice Hanne <patrice.hanne@expleogroup.com>
2023-12-22 16:42:29 +0100

    Ajoutez des fichiers projet.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace ExoKevin01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show(Calcul(), "Jour de semaine", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+
+        private string Calcul()
+        {
+            string[] joursSemaine = { "Erreur", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche" };
+
+            string valeur = this.inputUser.Text;
+
+            int indexJour;
+            if (int.TryParse(valeur, out indexJour) && indexJour >= 1 && indexJour <= 7)
+            {
+                return joursSemaine[indexJour];
+            }
+            else
+            {
+                return joursSemaine[0]; // "Erreur" si la valeur n'est pas valide
+            }
+        }
+
+    }
+}
